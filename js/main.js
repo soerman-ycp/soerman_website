36f25b67569788f3db2b5029ffb6937177862f7a
soerman-ycp <70239257+soerman-ycp@users.noreply.github.com>
2020-12-03 12:25:13 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "35%";
+    document.getElementById("main").style.marginLeft = "35%";
   }
   
   function closeNav() {
